eecaa646da48db938452d1d4c153c6a6a23ef271
Lawrence Gripper <info@grippers.co.uk>
2018-03-08 15:12:49 +0000

    Updated to correctly decode cert data

--- examples/log-analytics-container-monitoring/main.tf
+++ examples/log-analytics-container-monitoring/main.tf
@@ -69,12 +69,10 @@ resource "azurerm_log_analytics_solution" "test" {
 
 provider "kubernetes" {
   host     = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
-  username = "${azurerm_kubernetes_cluster.aks.kube_config.0.username}"
-  password = "${azurerm_kubernetes_cluster.aks.kube_config.0.password}"
-
-  client_certificate     = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate}"
-  client_key             = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_key}"
-  cluster_ca_certificate = "${azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate}"
+  
+  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)}"
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)}"
 }
 
 resource "kubernetes_namespace" "monitoring" {
